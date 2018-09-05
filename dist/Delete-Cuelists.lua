55ab3083548426f1969bf725a62356fb2e26a237
Spb8Lighting <sylvain.guiblain@gmail.com>
2018-09-06 01:23:54 +0200

    Add new script "Delete Cuelists"

--- dist/Delete-Cuelists.lua
+++ dist/Delete-Cuelists.lua
@@ -1,3 +1,31 @@
+-- ShowCockpit LUA Script: DeleteRangeOfCuelist
+--   created on ShowCockpit v2.4.2
+--   by Spb8 Lighting
+--   on 05-09-2018
+
+-------------
+-- Purpose --
+-------------
+-- This script allows to delete a range of cuelist (batch mode)
+
+---------------
+-- Changelog --
+---------------
+-- 05-09-2018 - 1.0: Creation
+
+-------------------
+-- Configuration --
+-------------------
+
+Settings = {
+	WaitTime = 0.05
+}
+
+ScriptInfos = {
+	version = "1.0",
+	name = "DeleteRangeOfCuelist"
+}
+
 -- ShowCockpit LUA Script: LuaHeader for Spb8 Lighting LUA Script
 
 ---------------
@@ -218,3 +246,70 @@ end
 
 HeadPrint()
 -- End of Header --
+
+
+
+----------------------------------------------------
+-- Main Script - dont change if you don't need to --
+----------------------------------------------------
+
+Content = {
+	StopMessage = "Stopped!" .. "\r\n\t" .. "The Preset type defined in the script configuration is not supported",
+	Done = "Deletion Ended!",
+	Options = "Delete Options:",
+	From = {
+		Question = "Delete from Cuelist n°",
+		Description = "Indicate the first Cuelist ID number (from cuelist repository)"
+	},
+	To = {
+		Question = "To Cuelist n°",
+		Description = "Indicate the last Cuelist ID number (from cuelist repository)"
+	},
+	Validation = {
+		Question = "Are you sure to delete following Cuelists?",
+		Description = "WARNING, it can't be UNDO! Use it with caution!"
+	}
+}
+-- Request the Start Preset ID n°
+InputSettings = {
+	Question = Content.From.Question,
+	Description = Content.From.Description,
+	Buttons = Form.OkCancel,
+	DefaultButton = Word.Ok,
+	Cancel = true
+}
+Settings.CLStart = InputNumber(InputSettings)
+if Cancelled(Settings.CLStart) then
+	goto EXIT
+end
+-- Request the Last Preset ID n°
+InputSettings.Question = Content.To.Question
+InputSettings.Description = Content.To.Description
+Settings.CLEnd = InputNumber(InputSettings)
+if Cancelled(Settings.CLEnd) then
+	goto EXIT
+end
+
+LogActivity(Content.Options)
+LogActivity("\r\n\t" .. "- From Cuelist n°" .. Settings.CLStart)
+LogActivity("\r\n\t" .. "- To Cuelist n°" .. Settings.CLEnd)
+
+InputValidationSettings = {
+	Question = Content.Validation.Question,
+	Description = Content.Validation.Description .. "\n\r\n\r" .. GetActivity(),
+	Buttons = Form.YesNo,
+	DefaultButton = Word.Yes
+}
+Settings.Validation = InputYesNo(InputValidationSettings)
+
+if Settings.Validation then
+	for CLNum = Settings.CLStart, Settings.CLEnd do
+		Onyx.DeleteCuelist(CLNum)
+		Sleep(Settings.WaitTime)
+	end
+	FootPrint(Content.Done)
+else
+	Cancelled()
+end
+
+::EXIT::
--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,26 @@ const gulp = require('gulp')
 	, merge = require('merge-stream')
 	, zip = require('gulp-zip')
 	, fs = require('fs')
-	, SourceLUA = ['./scripts/*.lua']
-	, DeletePresets = './scripts/Delete-Presets/Delete-Presets.lua'
 	, PresetsType = ['Color', 'PanTilt', 'Intensity', 'Gobo', 'Beam', 'BeamFX']
 
-// Generate all LUA Script with HEADER include
 gulp.task('build', () => {
-	return gulp.src(SourceLUA)
+	return gulp.src('./scripts/*.lua')
 		.pipe(replace('--##LUAHEADERINCLUDE##--', fs.readFileSync('./assets/header.lua', 'utf8') + "\n"))
-		.pipe(gulp.dest('dist/'))
-})
-gulp.task('build:DeletePresets', () => {
-	let tasks = PresetsType.map((Preset) => {
-		return gulp.src(DeletePresets)
-			.pipe(rename('Delete-Presets/Delete-Presets-' + Preset + '.lua'))
-			.pipe(replace('--##PRESET##--', Preset))
-			.pipe(replace('--##PRESETTYPE##--', 'PresetType = "' + Preset + '"'))
-			.pipe(replace('--##LUAHEADERINCLUDE##--', fs.readFileSync('./assets/header.lua', 'utf8') + "\n"))
-			.pipe(gulp.dest('dist/'))
-	})
-	return merge(tasks)
+		.pipe(gulp.dest('./dist/'))
 })
 
 gulp.task('release:zip', () => {
 	fs.unlink('./release/Onyx-Lua-ShowCockpit.zip', () => {
 		return gulp.src('./dist/**/*')
 			.pipe(zip('Onyx-Lua-ShowCockpit.zip'))
-			.pipe(gulp.dest('release/'))
+			.pipe(gulp.dest('./release/'))
 	})
 })
 
 // On any modification of dist file
 gulp.task('watch', () => {
-	gulp.watch(SourceLUA, ['build:DeletePresets'])
+	gulp.watch('./scripts', ['build', 'build'])
 })
 // Default task when gulp command launched
-gulp.task('default', ['build:DeletePresets'], () => {
+gulp.task('default', ['build'], () => {
 })
