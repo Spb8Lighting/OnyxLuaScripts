49385494c501e65258266ffdc9a33a1e80a105ab
Spb8Lighting <sylvain.guiblain@gmail.com>
2018-09-06 00:27:25 +0200

    Add ZIP Release function

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp')
-	, replace  = require('gulp-replace')
-	, rename  = require('gulp-rename')
-	, merge  = require('merge-stream')
+	, replace = require('gulp-replace')
+	, rename = require('gulp-rename')
+	, merge = require('merge-stream')
+	, zip = require('gulp-zip')
 	, fs = require('fs')
 	, SourceLUA = ['./scripts/*.lua']
 	, DeletePresets = './scripts/Delete-Presets/Delete-Presets.lua'
@@ -14,16 +15,25 @@ gulp.task('build', () => {
 		.pipe(gulp.dest('dist/'))
 })
 gulp.task('build:DeletePresets', () => {
-    let tasks = PresetsType.map((Preset)  => {
+	let tasks = PresetsType.map((Preset) => {
 		return gulp.src(DeletePresets)
 			.pipe(rename('Delete-Presets/Delete-Presets-' + Preset + '.lua'))
 			.pipe(replace('--##PRESET##--', Preset))
 			.pipe(replace('--##PRESETTYPE##--', 'PresetType = "' + Preset + '"'))
 			.pipe(replace('--##LUAHEADERINCLUDE##--', fs.readFileSync('./assets/header.lua', 'utf8') + "\n"))
 			.pipe(gulp.dest('dist/'))
-    })
-    return merge(tasks)
+	})
+	return merge(tasks)
 })
+
+gulp.task('release:zip', () => {
+	fs.unlink('./release/Onyx-Lua-ShowCockpit.zip', () => {
+		return gulp.src('./dist/**/*')
+			.pipe(zip('Onyx-Lua-ShowCockpit.zip'))
+			.pipe(gulp.dest('release/'))
+	})
+})
+
 // On any modification of dist file
 gulp.task('watch', () => {
 	gulp.watch(SourceLUA, ['build:DeletePresets'])
