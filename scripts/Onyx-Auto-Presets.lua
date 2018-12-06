cd8c434507be89a4473ce043401e90a66aab6171
GUIBLAIN <NG0051B0@EU.AIRBUS.CORP>
2018-12-06 16:09:10 +0100

    Continue the Auto-Preset initialisation

--- scripts/Onyx-Auto-Presets.lua
+++ scripts/Onyx-Auto-Presets.lua
@@ -11,6 +11,7 @@
 ---------------
 -- Changelog --
 ---------------
+-- 06-12-2018 - 0.0.0.0.0.1: Initialisation, not working yet
 -- 16-11-2018 - 0.0.0.0.0.0.1: Initialisation, not working yet
 
 -------------------
@@ -26,7 +27,7 @@ Settings = {
 }
 
 ScriptInfos = {
-	version = "0.0.0.0.0.0.1",
+	version = "0.0.0.0.0.1",
 	name = "AutoPresets"
 }
 
@@ -41,6 +42,7 @@ ScriptInfos = {
 --------------------------
 
 Rep = "%VAR%"
+RepID = "%GROUPID%"
 
 Content = {
   StopMessage = "Stopped!" .. "\r\n\t" .. "The value defined in the script configuration is not supported",
@@ -49,6 +51,23 @@ Content = {
     Description = "Please select what you want to do:",
     Create = "Create",
     Populate = "Populate"
+  },
+  Groups = {
+    ReuseGroup = "Do you want to keep your previous groups definition?",
+    Options = "Groups Options:",
+    List = "Group list:",
+    Question = "How many fixture groups will be used?",
+    Description = "Please indicate the quantity of groups where to populate presets:"
+  },
+  GroupID = {
+    Question = "What is the Group n°".. Rep .." ID?",
+    Description = "Please indicate the Group n°".. Rep .." ID:"
+  },
+  Resolution = {
+    Question = "[" .. Rep .. "] What is the " .. Rep .. " resolution for the group ".. RepID .."?",
+    Description = "[" .. Rep .. "] Please select the " .. Rep .. " resolution for the group ".. RepID ..":",
+    Standard = "8bits (standard)",
+    Fine = "16Bits (fine)"
   },
   Create = {
     Question = "Which type of preset do you want to " .. Rep .. "?",
@@ -102,6 +121,7 @@ end
 
 --##LUAPRESETSINCLUDE##--
 
+-- START POINT, to loop actions
 ::START::
 
 --# REQUEST the Action type # --
@@ -162,7 +182,7 @@ else
   LogActivity("\r\n\t" .."Preset Type: " .. Settings.Type)
 end
 
--- If Color preferencens not already set, and PresetType is Color, request the color preferences to apply
+-- If Color preference is not already set, and PresetType is Color, request the color preferences to be applied
 if Settings.Color == nil then
   if Settings.Type == PresetName.Color then
     InputSettings = false
@@ -185,10 +205,107 @@ if Settings.Color == nil then
   end
 end
 
+--# REQUEST the Fixtures Group # --
+-----------------------------------
+
+if Settings.Groups == nil then
+  ::GROUPS::
+  -- Request EU number of groups to be threated
+  InputSettings = false
+  InputSettings = {
+    Question = Content.Groups.Question,
+    Description = Content.Groups.Description,
+    Buttons = Form.OkCancel,
+    DefaultButton = Word.Ok,
+    Cancel = true
+  }
+
+  Settings.NbOfGroups = InputNumber(InputSettings)
+
+  if Cancelled(Settings.NbOfGroups) then
+    goto EXIT
+  end
+
+  -- Indicate the number of Groups to be threated
+  Settings.Groups = {}
+
+  -- Preset Resolution to get
+  PresetsResolution = {PresetName.Intensity, PresetName.Color}
+
+  -- Request EU details for each group
+  for i = 1, Settings.NbOfGroups, 1 do
+    -- Request the Group ID
+    InputSettings.Question = replace(Content.GroupID.Question, Rep, i),
+    InputSettings.Description = replace(Content.GroupID.Description, Rep, i)
+
+    local Group = {}
+    Group.Resolution = {}
+    Group.ID = InputNumber(InputSettings)
+
+    if Cancelled(GroupID) then
+        goto EXIT
+    end
+
+    -- Get the Group Name
+    Group.Name = Onyx.GetGroupName(Group.ID)
+
+    -- Request EU details for each PRESET TYPE resolution
+    InputSettings = false
+    InputSettings = {
+      Buttons = Form.OkCancel,
+      DefaultButton = Word.Ok,
+      DropDown = {Content.Resolution.Standard, Content.Resolution.Fine},
+      DropDownDefault = Content.Resolution.Standard,
+      Cancel = true
+    }
+    for idPreset, LocalPreset in pairs(PresetsResolution) do
+      ReplaceRepID = "n°" .. i .. " ID: " .. Group.ID .. " Name: " .. Group.Name
+      InputSettings.Question = replace(replace(Content.Resolution.Question, Rep, LocalPreset), RepID, ReplaceRepID),
+      InputSettings.Description = replace(replace(Content.Resolution.Description, Rep, LocalPreset), ReplaceRepID)
+
+      Group.Resolution[LocalPreset] = InputDropDown(InputSettings)
+
+        -- If not PresetType defined, exit
+      if Cancelled(Group[LocalPreset]) then
+        goto EXIT
+      end
+    end
+    -- Inset into the global Groups table
+    table.insert(Settings.Groups, Group)
+
+    -- RESUME of GROUPS
+    LogActivity(Content.Groups.Options)
+    LogActivity("\r\n\t" .. "- " .. Settings.NbOfGroups .. " group(s)")
+
+    -- DETAIL of GROUPS
+    LogActivity("\r\n" .. Content.Groups.List)
+    for i, Group in pairs(Settings.Groups) do
+        LogActivity("\r\n\t" .. "- n°" .. Group.id .. " - " .. Group.name)
+        for Key, Value in pairs(Group.Resolution) do
+          LogActivity("\r\n\t\t" .. "- " .. Key .. ": " .. Value)
+        end
+    end
+  end
+else
+  InputSettings = false
+  InputSettings = {
+    Question = Content.Groups.ReuseGroup,
+    Description = Content.Groups.ReuseGroup,
+    Buttons = Form.YesNo,
+    DefaultButton = Word.Yes
+  }
+
+  if InputYesNo(InputValidationSettings) then
+  else 
+    goto GROUPS
+  end
+end
+
 ----------------------------
 -- Execution for Creation --
 ----------------------------
 
+-- CREATE ACTION
 if Settings.Action == Content.Action.Create then
   --# USER Validation # --
   ------------------------
@@ -203,9 +320,16 @@ if Settings.Action == Content.Action.Create then
   Settings.Validation = InputYesNo(InputValidationSettings)
 
   if Settings.Validation then
+    if Settings.Type == PresetName.Color then
+    end
+
+    -- Don't exit, purpose to continue for a next action!
     goto START
   end
+-- POPULATE ACTION
 elseif Settings.Action == Content.Action.Populate then
+
+  -- Don't exit, purpose to continue for a next action!
   goto START
 end
 
