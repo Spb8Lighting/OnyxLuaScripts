0a8c514549f89ef440f0ee8f02cf86bdf1e6c282
Spb8Lighting <sylvain.guiblain@gmail.com>
2019-01-05 15:30:39 +0100

    Automatisation Update:  - Allow to watch independant script to avoid unwanted global update  - Allow to build independant script to avoid unwanted global update

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,58 @@ const gulp = require('gulp')
 	, merge = require('merge-stream')
 	, zip = require('gulp-zip')
 	, fs = require('fs')
-
-gulp.task('build', () => {
-	return gulp.src('./scripts/*.lua')
-		.pipe(replace('--##LUAHEADERINCLUDE##--', fs.readFileSync('./assets/header.lua', 'utf8') + "\n"))
-		.pipe(replace('--##LUAPRESETSINCLUDE##--', fs.readFileSync('./assets/Auto-Presets.lua', 'utf8') + "\n"))
-		.pipe(gulp.dest('./dist/'))
-})
-
+const Scripts = {
+	AutoPresets: {
+		LUA: 'Onyx-Auto-Presets.lua',
+		Header : {
+			Match : '--##LUAPRESETSINCLUDE##--',
+			Replace : 'Auto-Presets.lua'
+		}
+	},
+	CreatePlaybackFromPresets: {
+		LUA: 'Onyx-Create-Playbacks-from-Presets.lua'
+	},
+	DeleteCuelists: {
+		LUA: 'Onyx-Delete-Cuelists.lua'
+	},
+	DeleteGroups: {
+		LUA: 'Onyx-Delete-Groups.lua'
+	},
+	DeletePresets: {
+		LUA: 'Onyx-Delete-Presets.lua'
+	},
+	RenameCuelists: {
+		LUA: 'Onyx-Rename-Cuelists.lua'
+	},
+	UpdateCueFadeCuelistRelease: {
+		LUA: 'Onyx-Update-CueFade-CuelistRelease.lua'
+	}
+}
+let Specific = {
+	Builder: (ScriptObj, ScriptName) => {
+		gulp.task('build:' + ScriptName, () => {
+			if (ScriptObj.Header) {
+				return gulp.src('./scripts/' + ScriptObj.LUA)
+					.pipe(replace('--##LUAHEADERINCLUDE##--', fs.readFileSync('./assets/header.lua', 'utf8') + "\n"))
+					.pipe(replace(ScriptObj.Header.Match, fs.readFileSync('./assets/' + ScriptObj.Header.Replace, 'utf8') + "\n"))
+					.pipe(gulp.dest('./dist/'))
+			} else {
+				return gulp.src('./scripts/' + ScriptObj.LUA)
+					.pipe(replace('--##LUAHEADERINCLUDE##--', fs.readFileSync('./assets/header.lua', 'utf8') + "\n"))
+					.pipe(gulp.dest('./dist/'))
+			}
+		})
+	},
+	Watcher: (ScriptObj, ScriptName) => {
+		gulp.task('watch:' + ScriptName, () => {
+			gulp.watch('./scripts/' + ScriptObj.LUA, ['build:' + ScriptName])
+			if (ScriptObj.Header) {
+				gulp.watch('./assets/' + ScriptObj.Header, ['build:' + ScriptName])
+			}
+		})
+	}
+}
+// Create Release Archive Build
 gulp.task('release:zip', () => {
 	fs.unlink('./release/Onyx-Lua-ShowCockpit.zip', () => {
 		return gulp.src('./dist/**/*')
@@ -28,3 +72,14 @@ gulp.task('watch', () => {
 // Default task when gulp command launched
 gulp.task('default', ['build', 'watch'], () => {
 })
+// Generic Build
+gulp.task('build', () => {
+	for (let Script in Scripts) {
+		gulp.start('build:' + Script);
+	}
+})
+// Generate all specific builder and watcher per script
+for (let Script in Scripts) {
+	Specific.Builder(Scripts[Script], Script)
+	Specific.Watcher(Scripts[Script], Script)
+}
